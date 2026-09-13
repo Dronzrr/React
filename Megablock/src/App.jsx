48e6e47af435501773c311f22d6a97f6042cce37
Bittu000 <bittuparmar000@gmail.com>
2026-09-13 22:06:17 +0530

    Update React projects

--- Megablock/src/App.jsx
+++ Megablock/src/App.jsx
@@ -1,15 +1,42 @@
 
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header } from './components'
+import { Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>A blog app</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-col bg-gray-950 text-white'>
+      <Header />
+
+      <main className='flex-1'>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  ) : null
+
 }
 
 export default App
